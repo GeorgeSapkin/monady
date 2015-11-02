89cac563f5807f0187219dbad84c1f4ee6ccde67
George Sapkin <george.sapkin@gmail.com>
2015-11-02 11:13:18 +0100

    Better identity and just tests

--- test/identity.js
+++ test/identity.js
@@ -1,54 +1,79 @@
 'use strict'
 
 const assert = require('assert');
+const co     = require('co');
 
 const F = require('..');
 
+const Identity = F.Identity;
+const identity = F.identity;
+
 describe('Identity', () => {
     describe('call constructor', () => {
-        describe('with value', () => {
-            it('should work', () => {
-                assert.doesNotThrow(() => new F.Identity(5));
+        describe('should work', () => {
+            it('with value', () => {
+                assert.doesNotThrow(() => new Identity(5));
             });
         });
 
-        describe('without value', () => {
-            it('should throw', () => {
-                assert.throws(() => new F.Identity());
+        describe('should throw', () => {
+            it('without value', () => {
+                assert.throws(() => new Identity());
+            });
+
+            it('with null', () => {
+                assert.throws(() => new Identity(null));
             });
         });
     });
 
     describe('create through factory method', () => {
-        describe('with value', () => {
-            it('should work', () => {
-                assert.doesNotThrow(() => F.identity(5));
+        describe('should work', () => {
+            it('with value', () => {
+                assert.doesNotThrow(() => identity(5));
             });
         });
 
-        describe('without value', () => {
-            it('should throw', () => {
-                assert.throws(() => F.identity());
+        describe('should throw', () => {
+            it('without value', () => {
+                assert.throws(() => identity());
+            });
+
+            it('with null', () => {
+                assert.throws(() => identity(null));
             });
         });
     });
 
     describe('get value', () => {
         it('should return same value', () => {
-            assert.equal(F.identity(5).value, 5);
+            assert.equal(identity(5).value, 5);
         });
     });
 
     describe('call then', () => {
-        describe('with transform', () => {
-            it('should work', () => {
-                assert.equal(F.identity(5).then(a => a + 3), 8);
+        describe('should work', () => {
+            it('with transform', () => {
+                assert.equal(identity(5).then(a => a + 3), 8);
             });
         });
 
-        describe('without transform', () => {
-            it('should throw', () => {
-                assert.throws(F.identity(5).then);
+        describe('should throw', () => {
+            it('without transform', () => {
+                assert.throws(identity(5).then);
+            });
+        });
+    });
+
+    describe('function*', () => {
+        describe('should yield same value', () => {
+            it('when not nothing', () => {
+                return co(function* () {
+                    return identity(5);
+                }).then(
+                    val => assert.strictEqual(val, 5),
+                    err => assert.ifError(err)
+                );
             });
         });
     });
--- test/just.js
+++ test/just.js
@@ -1,54 +1,79 @@
 'use strict'
 
 const assert = require('assert');
+const co     = require('co');
 
 const F = require('..');
 
+const Just = F.Just;
+const just = F.just;
+
 describe('Just', () => {
     describe('call constructor', () => {
-        describe('with value', () => {
-            it('should work', () => {
-                assert.doesNotThrow(() => new F.Just(5));
+        describe('should work', () => {
+            it('with value', () => {
+                assert.doesNotThrow(() => new Just(5));
             });
         });
 
-        describe('without value', () => {
-            it('should throw', () => {
-                assert.throws(() => new F.Just());
+        describe('should throw', () => {
+            it('without value', () => {
+                assert.throws(() => new Just());
+            });
+
+            it('with null', () => {
+                assert.throws(() => new Just(null));
             });
         });
     });
 
     describe('create through factory method', () => {
-        describe('with value', () => {
-            it('should work', () => {
-                assert.doesNotThrow(() => F.just(5));
+        describe('should work', () => {
+            it('with value', () => {
+                assert.doesNotThrow(() => just(5));
             });
         });
 
-        describe('without value', () => {
-            it('should throw', () => {
-                assert.throws(() => F.just());
+        describe('should throw', () => {
+            it('without value', () => {
+                assert.throws(() => just());
+            });
+
+            it('with null', () => {
+                assert.throws(() => just(null));
             });
         });
     });
 
     describe('get value', () => {
         it('should return same value', () => {
-            assert.equal(F.just(5).value, 5);
+            assert.equal(just(5).value, 5);
         });
     });
 
     describe('call then', () => {
-        describe('with transform', () => {
-            it('should work', () => {
-                assert.equal(F.just(5).then(a => a + 3), 8);
+        describe('should work', () => {
+            it('with transform', () => {
+                assert.equal(just(5).then(a => a + 3), 8);
             });
         });
 
-        describe('without transform', () => {
-            it('should throw', () => {
-                assert.throws(F.just(5).then);
+        describe('should throw', () => {
+            it('without transform', () => {
+                assert.throws(just(5).then);
+            });
+        });
+    });
+
+    describe('function*', () => {
+        describe('should yield same value', () => {
+            it('when not nothing', () => {
+                return co(function* () {
+                    return just(5);
+                }).then(
+                    val => assert.strictEqual(val, 5),
+                    err => assert.ifError(err)
+                );
             });
         });
     });
