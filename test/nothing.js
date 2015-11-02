e6693ddc2e236148120d2033211e248fca558cdd
George Sapkin <george.sapkin@gmail.com>
2015-11-02 10:48:38 +0100

    Better nothing tests

--- test/nothing.js
+++ test/nothing.js
@@ -1,25 +1,40 @@
 'use strict'
 
 const assert = require('assert');
+const co     = require('co');
 
 const F = require('..');
+
+const Nothing = F.Nothing;
+const nothing = F.nothing;
 
 describe('Nothing', () => {
     describe('call constructor', () => {
         it('should work', () => {
-            assert.doesNotThrow(() => new F.Nothing(), TypeError);
+            assert.doesNotThrow(() => new Nothing(), TypeError);
         })
     });
 
     describe('read predefined constant', () => {
         it('should work', () => {
-            assert.notEqual(F.nothing, null);
+            assert.notEqual(nothing, null);
         })
     });
 
     describe('call then', () => {
         it('should fail', () => {
-            assert.throws(() => F.nothing.then(a => a), TypeError);
+            assert.throws(() => nothing.then(a => a), TypeError);
         })
+    });
+
+    describe('should yield nothing', () => {
+        it('when nothing', () => {
+            return co(function* () {
+                return nothing;
+            }).then(
+                val => assert.equal(val, nothing),
+                err => assert.ifError(err)
+            );
+        });
     });
 });
